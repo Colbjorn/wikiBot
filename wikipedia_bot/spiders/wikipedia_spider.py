7496931f46ee09f7bcb76a6250f1d1a5897a2b74
Colbjorn <hdafas@gmail.com>
2019-02-19 22:03:47 +0000

    According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!

--- wikipedia_bot/spiders/wikipedia_spider.py
+++ wikipedia_bot/spiders/wikipedia_spider.py
@@ -2,13 +2,19 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "wiki"
-    count = 0
     maxCount = 5
     historyHeadings = []
     historyUrls = []
     currentUrl = 'https://en.wikipedia.org/wiki/Athletics_at_the_2000_Summer_Olympics_%E2%80%93_Men%27s_20_kilometres_walk'
     goalUrl = 'https://en.wikipedia.org/wiki/Adolf_Hitler'
-
+    blacklist = [
+        'https://en.wikipedia.org/wiki/Portal:Contents',
+        'https://en.wikipedia.org/wiki/Help:Category',
+        'https://en.wikipedia.org/wiki/Special:MyTalk',
+        'https://en.wikipedia.org/wiki/Special:MyContributions',
+        'https://donate.wikimedia.org/wiki/Special:FundraiserRedirector?utm_source=donate&utm_medium=sidebar&utm_campaign=C13_en.wikipedia.org&uselang=en',
+        'https://en.wikipedia.org/wiki/Wikipedia:Contact_us'
+    ]
     def start_requests(self):
         urls = [
             'https://en.wikipedia.org/wiki/Athletics_at_the_2000_Summer_Olympics_%E2%80%93_Men%27s_20_kilometres_walk',
@@ -17,34 +23,43 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):  # Recursive pathfinder
+        print('''
+        
+        
+        ''')
+        print('Currently at ' + self.currentUrl)
+        print()
+        print('History: ')
+        print(self.historyUrls)
 
         # If the goal is reached, yields number of clicks and history.
         if self.currentUrl == self.goalUrl:
-            self.maxCount = self.count
+            self.maxCount = len(self.historyUrls)
             yield {
-                'Clicks': self.count,
+                'Clicks': len(self.historyUrls),
                 'History': self.historyHeadings
             }
-            self.count -= 1
             del self.historyHeadings[-1]
             del self.historyUrls[-1]
 
         # Update tracking, then end recursive branch.
-        elif self.count == self.maxCount:
-            self.count -= 1
+        elif len(self.historyUrls) == self.maxCount:
             del self.historyHeadings[-1]
             del self.historyUrls[-1]
 
-        elif self.count > self.maxCount:
+        elif len(self.historyUrls) > self.maxCount:
             yield 'MaxCount error'
 
         # Updates count and history, generates a list then iterates through it.
         else:
-            self.count += 1
             self.historyHeadings.append(response.css('h1::text').get())
             self.historyUrls.append(self.currentUrl)
 
-            for a in response.css('a'):
-                if self.currentUrl not in a:
-                    self.currentUrl = a
-                    yield response.follow(a, callback=self.parse)
+            for a in response.css('a').xpath('@href').extract():
+                if a is not None:
+                    a = response.urljoin(a)
+                    if self.currentUrl not in a:
+                        if a not in self.blacklist:
+                            if 'en.wikipedia.org' in a:
+                                self.currentUrl = a
+                                yield response.follow(a, callback=self.parse)
