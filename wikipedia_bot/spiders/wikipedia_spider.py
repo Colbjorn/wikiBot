0560aba592b0b466151a7755ac61f4cfcac9a860
Colbjorn <hdafas@gmail.com>
2019-02-19 14:39:52 +0000

    Why are we still here? Just to suffer? Every night, I can feel my leg… and my arm… even my fingers. The body I’ve lost… the comrades I’ve lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?

--- wikipedia_bot/spiders/wikipedia_spider.py
+++ wikipedia_bot/spiders/wikipedia_spider.py
@@ -1,24 +1,50 @@
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = "wiki"
+    count = 0
+    maxCount = 5
+    historyHeadings = []
+    historyUrls = []
+    currentUrl = 'https://en.wikipedia.org/wiki/Athletics_at_the_2000_Summer_Olympics_%E2%80%93_Men%27s_20_kilometres_walk'
+    goalUrl = 'https://en.wikipedia.org/wiki/Adolf_Hitler'
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
+            'https://en.wikipedia.org/wiki/Athletics_at_the_2000_Summer_Olympics_%E2%80%93_Men%27s_20_kilometres_walk',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):  #Sample code for parsing
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+    def parse(self, response):  # Recursive pathfinder
 
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        # If the goal is reached, yields number of clicks and history.
+        if self.currentUrl == self.goalUrl:
+            self.maxCount = self.count
+            yield {
+                'Clicks': self.count,
+                'History': self.historyHeadings
+            }
+            self.count -= 1
+            del self.historyHeadings[-1]
+            del self.historyUrls[-1]
+
+        # Update tracking, then end recursive branch.
+        elif self.count == self.maxCount:
+            self.count -= 1
+            del self.historyHeadings[-1]
+            del self.historyUrls[-1]
+
+        elif self.count > self.maxCount:
+            yield 'MaxCount error'
+
+        # Updates count and history, generates a list then iterates through it.
+        else:
+            self.count += 1
+            self.historyHeadings.append(response.css('h1::text').get())
+            self.historyUrls.append(self.currentUrl)
+
+            for a in response.css('a'):
+                if self.currentUrl not in a:
+                    self.currentUrl = a
+                    yield response.follow(a, callback=self.parse)
